6c759f2a08ef2c6305b223a9ecf2857f4e092e70
bkmnst <bakemonsuta@gmail.com>
2023-12-21 23:24:08 +0200

    add chrome installation

--- fedora/apps.sh
+++ fedora/apps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #dnf repos
+sudo dnf config-manager --set-enabled google-chrome
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -15,7 +16,7 @@ sudo dnf group upgrade --with-optional Multimedia -y
 sudo dnf groupupdate sound-and-video -y
 
 #dnf apps
-sudo dnf install transmission go neovim python3-neovim ffmpegthumbnailer gnome-tweaks adw-gtk3-theme code nicotine+ -y
+sudo dnf install transmission go neovim python3-neovim ffmpegthumbnailer gnome-tweaks adw-gtk3-theme code nicotine+ google-chrome-stable -y
 
 #flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
