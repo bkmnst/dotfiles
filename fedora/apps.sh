9211b5a54777f8e6411874784e57f045a289aba6
bkmnst <bakemonsuta@gmail.com>
2023-12-21 23:22:25 +0200

    add more comments & separation

--- fedora/apps.sh
+++ fedora/apps.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+#dnf repos
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo dnf groupupdate core -y
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install transmission go neovim python3-neovim ffmpegthumbnailer gnome-tweaks adw-gtk3-theme code nicotine+ -y
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
-sudo dnf install lame\* --exclude=lame-devel -y
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-sudo dnf group upgrade --with-optional Multimedia -y
-sudo dnf groupupdate sound-and-video -y
 sudo dnf group remove libreoffice
 sudo dnf remove firefox totem libreoffice-core -y
+sudo dnf groupupdate core -y
+
+#codecs (without replacing mesa)
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf install lame\* --exclude=lame-devel -y
+sudo dnf group upgrade --with-optional Multimedia -y
+sudo dnf groupupdate sound-and-video -y
+
+#dnf apps
+sudo dnf install transmission go neovim python3-neovim ffmpegthumbnailer gnome-tweaks adw-gtk3-theme code nicotine+ -y
 
 #flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -45,7 +50,7 @@ flatpak install flathub io.gitlab.news_flash.NewsFlash -y
 flatpak install flathub io.github.seadve.Mousai -y
 flatpak install flathub org.nickvision.tagger -y
 
-#media
+#tools
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub org.blender.Blender -y
