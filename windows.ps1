15abfed5ce9547456096a57d69a96d5ddeb68c06
bkmnst <49692481+bkmnst@users.noreply.github.com>
2024-07-01 20:48:52 +0300

    why bother with hiding it if i didn't do it anyway kekw

--- windows.ps1
+++ windows.ps1
@@ -15,6 +15,7 @@ git config --global credential.helper manager-core
 git config --global init.defaultBranch main
 git config --global color.ui auto
 git config --global user.name "bkmnst"
+git config --global user.email "bakemonsuta@gmail.com"
 go env -w GOPATH=$HOME/.go
 
 #wsl
